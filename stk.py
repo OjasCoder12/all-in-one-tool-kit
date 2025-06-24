934de14ff11b59afdcc5ab347b3090636bd25a0d
OjasCoder12 <ojas.solanki12@gmail.com>
2025-06-24 17:13:02 +0530

    Add files via upload

---
+++ stk.py
@@ -0,0 +1,118 @@
+# ---------------- GUI Libraries ----------------
+from tkinter import *
+from kivy.app import *
+from customtkinter import *
+from dearpygui.core import *
+from dearpygui.simple import *
+
+# ---------------- Data & File Handling ----------------
+from numpy import *
+from pandas import *
+from openpyxl import *
+from xlrd import *
+from json import *
+from xml.etree.ElementTree import *
+from yaml import *
+from os import *
+from pathlib import *
+from shutil import *
+from datetime import *
+from time import *
+
+# ---------------- Visualization ----------------
+from matplotlib.pyplot import *
+from seaborn import *
+from plotly import *
+from bokeh import *
+from tabulate import *
+
+# ---------------- Machine Learning ----------------
+from sklearn.model_selection import *
+from sklearn.preprocessing import *
+from sklearn.metrics import *
+from sklearn.pipeline import *
+from sklearn.decomposition import *
+from sklearn.ensemble import *
+from sklearn.linear_model import *
+from sklearn.svm import *
+from sklearn.tree import *
+from sklearn.neighbors import *
+from sklearn.cluster import *
+from sklearn.feature_extraction import *
+from sklearn.impute import *
+from sklearn.compose import *
+from sklearn.feature_selection import *
+from sklearn.manifold import *
+from sklearn.gaussian_process import *
+from sklearn.discriminant_analysis import *
+from xgboost import *
+from lightgbm import *
+
+# ---------------- Deep Learning ----------------
+from tensorflow import *
+from keras import *
+from torch import *
+from transformers import *
+
+# ---------------- NLP & Text ----------------
+from nltk import *
+from spacy import *
+from textblob import *
+from langdetect import *
+from keybert import *
+
+# ---------------- Automation & Voice ----------------
+from pyautogui import *
+from keyboard import *
+from pynput.keyboard import *
+from schedule import *
+from playsound import *
+from pyttsx3 import *
+from gtts import *
+from speech_recognition import *
+
+# ---------------- OpenCV / Image / Video ----------------
+from cv2 import *
+from mediapipe import *
+from PIL import *
+from moviepy.editor import *
+
+# ---------------- Terminal / Dev Tools ----------------
+from typer import *
+from rich import *
+from loguru import *
+from argparse import *
+from black import *
+from isort import *
+from flake8 import *
+from pylint import *
+from pre_commit import *
+from watchdog.observers import *
+from watchdog.events import *
+
+# ---------------- Networking & Web ----------------
+from requests import *
+from aiohttp import *
+from flask import *
+from fastapi import *
+from httpx import *
+from websocket import *
+from pyngrok import *
+
+# ---------------- Game & Creative ----------------
+from pygame import *
+from turtle import *
+from manim import *
+
+# ---------------- Packaging ----------------
+from setuptools import *
+from wheel import *
+from twine import *
+from build import *
+from poetry import *
+
+# ---------------- Terminal Fun ----------------
+from pyfiglet import *
+from art import *
+from termcolor import *
+from colorama import *
